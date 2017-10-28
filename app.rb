32591ddaf7c467ded294412b796e153399e91430
Oliver Denman <oliver@therewillbeanimals.com>
2017-10-28 10:22:52 +0100

    Initial scratchings

---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'pry'
+require 'twitter'
+
+Dir['./lib/*.rb'].each { |file| require file }
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+end
+
+class Tweeter
+  def run
+    rep = GhostReport.new(words)
+    client.update(rep.tweet) if rep.tweet.length <= 144
+    # puts rep.tweet
+  end
+
+  private
+
+  def words
+    @words ||= Dir['./data/*'].inject({}) do |h, file|
+      h.merge(File.basename(file).to_sym => lines(file))
+    end
+  end
+
+  def lines(file)
+    File.readlines(file).map(&:chomp).reject(&:empty?)
+  end
+end
+
+tweeter = Tweeter.new
+tweeter.run
+
+loop do
+  sleep 1800
+  tweeter.run
+end
---
+++ lib/ghost_report.rb
@@ -0,0 +1,74 @@
+# Represents a UK Ghost Report tweet
+class GhostReport
+  def initialize(words)
+    words.keys.each do |k|
+      define_singleton_method(singular(k), -> { words[k].sample })
+    end
+  end
+
+  def tweet
+    [
+      "#{sentence}. #{hash_tags}",
+      "#{sentence}. #{additional_note}. #{hash_tags}"
+    ].sample
+  end
+
+  private
+
+  attr_accessor :words
+
+  def hash_tags
+    "\##{town.delete(' ')} \##{town.delete(' ')}#{location.split.map(&:capitalize).join}"
+  end
+
+  def singular(plural)
+    plural.to_s.chomp('s').to_sym
+  end
+
+  def sentence
+    sentence_structure.join(' ')
+  end
+
+  # TODO: Extract this to data file
+  def sentence_structure
+    [
+      [
+        witness,
+        witness_present_verb,
+        ghost_adjective,
+        ghost_noun, preposition,
+        town,
+        location
+      ],
+      [
+        ['Reports of', 'Sightings of'].sample,
+        ghost_adjective,
+        ghost_noun,
+        preposition,
+        town,
+        location
+      ],
+      [
+        'Hauntings',
+        %w[reported recorded].sample,
+        preposition,
+        town,
+        location
+      ]
+    ].sample
+  end
+
+  def witness
+    witness_noun.capitalize
+  end
+
+  # Need to memoize these otherwise hash tag town and location
+  # differ from those in main tweet content
+  def town
+    @town ||= town_name
+  end
+
+  def location
+    @location ||= location_name
+  end
+end
