d127a26f4f70051cd9b44a754e0df39e9b6ad98b
Oliver Denman <oliver@therewillbeanimals.com>
2017-10-28 11:12:55 +0100

    Move twitter client setup into Tweeter
    Client was being set up outside class so Tweeter could not see it.

--- app.rb
+++ app.rb
@@ -3,13 +3,6 @@ require 'twitter'
 
 Dir['./lib/*.rb'].each { |file| require file }
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
-end
-
 class Tweeter
   def run
     rep = GhostReport.new(words)
@@ -27,6 +20,15 @@ class Tweeter
 
   def lines(file)
     File.readlines(file).map(&:chomp).reject(&:empty?)
+  end
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+    end
   end
 end
 
